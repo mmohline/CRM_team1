fd7df62d1ac9b7ed64fc97ef06a07ad157965936
mmohline <119363747+mmohline@users.noreply.github.com>
2022-11-30 18:51:47 -0500

    edited test file

--- git_push_test.py
+++ git_push_test.py
@@ -1,4 +1,7 @@
-db_define_table("test push",
+db.define_table("test push",
                 Field("test1")
                 )
-                
+
+db.define_table("test push updated code",
+                Field("test1")
+                )

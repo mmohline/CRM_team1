c53ee6b656245f68b876f5117a3bd4610b855dce
EricUrbansky <118932588+EricUrbansky@users.noreply.github.com>
2022-11-30 14:58:37 -0500

    Add files via upload
    cleaned it up a little bit starting working on the references and field types. Got them decent for now but I'm going to work on a better view.

--- db_tables.py
+++ db_tables.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
-db.define_table(
-    "contacts",
-    Field("first_name"),
-    Field("last_name"),
-    Field("company_name"),
-    Field("address"),
-    Field("city"),
-    Field("state_name"),
-    Field("zip"),
-    Field("phone1"),
-    Field("phone2"),
-    Field("email", requires=IS_EMAIL()),
-    )
+db.define_table("region",
+                Field("state_name"),
+                Field("state_abbv"),
+                Field("city"),
+                Field("zip")
+                )
 
-
-
+db.define_table("locations",
+                Field("address"),
+                Field("company_name"),
+                Field("office_location"),
+                Field("state_abbv"),
+                Field("city")
+                )
 
 db.define_table("companines",
-                Field("name"),
-                Field("address"),
-                Field("date_time")
+                Field("company_name", "references locations"),
+                Field("address", "references locations"),
+                Field("date_created", requires=IS_DATE("%d/%m/%Y"))
                 )
 
 db.define_table("lead_source",
@@ -27,17 +24,38 @@ db.define_table("lead_source",
                 Field("walk_in"),
                 Field("on_call"),
                 Field("mail"),
-                Field("email", requires=IS_EMAIL())
+                Field("email")
                 )
 
-db.define_table("locations",
-                Field("address"),
-                Field("company_name"),
-                Field("main_phone"),
-                Field("sic_number"),
-                Field("state_abbv"),
-                Field("city")
+
+
+
+db.define_table(
+    "contacts",
+    Field("first_name", notnull = True),
+    Field("last_name", notnull = True),
+    Field("company_name", "references locations"),
+    Field("address"), # company address
+    Field("city", "references region"),
+    Field("state_name", "references region"),
+    Field("zip", "references region"),
+    Field("phone1", requires = IS_EMPTY_OR(IS_MATCH('[\d\-\(\) ]+'))),
+    Field("phone2", requires = IS_EMPTY_OR(IS_MATCH('[\d\-\(\) ]+'))),
+    Field("email", requires=IS_EMAIL()),
+    Field("date_created", requires=IS_DATE("%d/%m/%Y")),
+    Field("office_location", "references locations"),
+    Field("office_phone"),
+    Field("office_email"),
+    Field("contact_type", "reference lead_source")
+    )
+
+
+
+db.define_table("games",
+                Field("game_name"),
+                Field("game_type")
                 )
+
 db.define_table("activities",
                 Field("direct_sale"),
                 Field("advertiser"),
@@ -46,12 +64,17 @@ db.define_table("activities",
                 )
 
 db.define_table("lifecycle",
-                Field("last_contacted")
+                Field("last_contacted", "reference contacts"),
+                Field("how_contacted"),
+                Field("game_of_intrest"),        #createTableForGames
+                Field("next_update"),
+                Field("game_bought", "reference games"),
+                Field("game_type", "reference games")
                 )
 
 db.define_table("active_sales",
                 Field("last_sale_date"),
                 Field("location_of_sale"),
-                Field("first_name"),
+                Field("first_name", "references contacts"),
                 Field("email", requires=IS_EMAIL())
                 )

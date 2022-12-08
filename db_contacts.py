023fbac266766b9355dfef0bddda224a2722b1c2
mmohline <119363747+mmohline@users.noreply.github.com>
2022-12-08 18:40:06 -0500

    udpate as of 12/8/22

--- db_contacts.py
+++ db_contacts.py
@@ -1,42 +1,43 @@
 import datetime
 
 db.define_table(
+    "company",
+    Field("name"),
+    format = '%(name)s'
+    )
+
+db.define_table(
     "states",
-    Field("state_name"),
-    format='%(state_name)s'
-    )
-
-db.define_table(
-    "companies",
-    Field("company"),
-    format='%(company)s'
-    )
-
-
-db.define_table(
-    "office_locations",
-    Field("Region")
+    Field("stateNames"),
+    format = '%(stateNames)s'
     )
 
 db.define_table(
     "contactType",
-    Field("contactType")
+    Field("contactType"),
+    format = '%(contactType)s'
     )
 
 db.define_table(
-    'contacts',
-    Field('firstName'),
-    Field('lastName'),
-    Field('cell', requires = IS_EMPTY_OR(IS_MATCH('[\d\-\(\) ]+')) ),
-    Field('email', requires = IS_EMPTY_OR(IS_EMAIL()) ),
-    Field('city'),
-    Field('state_name', 'reference states'),
-    Field('zip', requires = IS_EMPTY_OR(IS_LENGTH(5,5)) ),
-    Field('companyId', 'reference companies'),
-    Field('officeLocationId', 'reference office_locations'),
-    Field('officePhone', requires = IS_EMPTY_OR(IS_MATCH('[\d\-\(\) ]+')) ),
-    Field('title'),
-    Field('contactType', 'reference contactType'),
-    Field('createdDate', type='date'),
-    Field('lastChanged', type='date'),
-   )
+    "lifecycleStage",
+     Field("lifecycleStage"),
+    format = '%(lifecycleStage)s'
+    )
+
+
+db.define_table(
+    "contact",
+    Field("firstName", notnull=True),
+    Field("lastName"),
+    Field("company", "reference company"),
+    Field("email", requires=IS_EMAIL()),
+    Field("mobile"),
+    Field("address"),
+    Field("city"),
+    Field("states", "reference states"),
+    Field("jobTitle"),
+    Field("contactType", "reference contactType"),
+    Field("lifecycleStage", "reference lifecycleStage"),
+    Field("leadStatus", requires=IS_IN_SET(["active", "inactive"])),
+    Field("createdDate", type='datetime', default=datetime.datetime.now, requires=IS_DATE(format=('%Y-%m-%d')))
+    )
